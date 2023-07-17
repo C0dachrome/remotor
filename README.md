28734b2960ac1a14ab17a0c2956ca973d7111124
C0dachrome <132285309+C0dachrome@users.noreply.github.com>
2023-07-16 21:10:43 -0600

    Add Code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # remotor
  drive motors and servos from an rc reciever with arduino and raspberry pi with adafruit CRICKIT HAT.
+
+ 
---
+++ remotor.ino
@@ -0,0 +1,32 @@
+int baseline = 1489;
+const int CH1 = 11;
+const int CH2 = 10;
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+   float y = pulseIn(CH2, HIGH, 30000);
+   float x = pulseIn(CH1, HIGH, 30000);
+  
+  if(y > baseline){
+     int a = y - baseline;
+     x = x + a;
+  } else if(y < baseline){
+    int a = baseline - y;
+    x = x - a;
+  }
+  
+  if(x < baseline && y >= 1480){
+    int b = baseline - x;
+    y = y + b;
+  } else if(x > baseline && y <= 1510){
+    int b = x - baseline;
+    y = y - b;
+  }
+
+  
+   Serial.println(String(((x*pow(10,-4))*3)-0.45));
+   Serial.println(String(((y*pow(10,-4))*3)-0.45));
+  }
