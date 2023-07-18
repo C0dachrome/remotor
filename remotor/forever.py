cb6083ce419a0427d40573375765fe579cf4a5fe
C0dachrome <132285309+C0dachrome@users.noreply.github.com>
2023-07-18 14:08:01 -0600

    Update forever.py

--- remotor/forever.py
+++ remotor/forever.py
@@ -5,5 +5,5 @@ import sys
 filename = sys.argv[1]
 while True:
     print("\nStarting " + filename)
-    p = Popen("python " + filename, shell=True)
+    p = Popen("python " + filename, shell=True)  # restarts remotorX_XX.py when an error occurs.
     p.wait()
